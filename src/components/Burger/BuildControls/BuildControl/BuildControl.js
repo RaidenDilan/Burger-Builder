7d946d41f6d986dfd122096c2a7dca0ec55b2fd4
RaidenDilan <raiden18@me.com>
2020-02-10 23:37:50 +0000

    Outputting Multiple Build Controls

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import classes from './BurgerControl.css';
+import classes from './BuildControl.css';
 
 const burgerControl = (props) => (
   // label => ingredientLabel
