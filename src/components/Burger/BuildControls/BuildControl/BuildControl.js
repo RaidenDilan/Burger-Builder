d0425e61797a3de317c6146cd716d9d7ec367b56
RaidenDilan <raiden18@me.com>
2020-02-10 23:54:36 +0000

    Connecting State to Build Controls

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 import classes from './BuildControl.css';
 
-const burgerControl = (props) => (
+const buildControl = (props) => (
   // label => ingredientLabel
   <div className={ classes.BuildControl }>
     <div className={ classes.Label }>{ props.label }</div>
     <button className={ classes.Less }>Less</button>
-    <button className={ classes.More }>More</button>
+    <button className={ classes.More } onClick={ props.added }>More</button>
   </div>
 );
 
-export default burgerControl;
+export default buildControl;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,14 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+// Typpical global constants are written in ALL CAPS
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+}
+
 class BurgerBuilder extends Component {
   // constructor(props) {
   //   super(props);
@@ -16,14 +24,36 @@ class BurgerBuilder extends Component {
       bacon: 0,
       cheese: 0,
       meat: 0
-    }
+    },
+    totalPrice: 4
+  };
+
+  addIngredientHandler = (type) => {
+    const oldCOunt = this.state.ingredients[type];
+    const updatedCount = oldCOunt + 1;
+    const updatedIngredients = {
+      // State should be updated in an unmutable way
+      // Then use the spread opertar to distribute the properties of the old ingredients state into the new object we are creatng bellow.
+      ...this.state.ingredients
+    };
+
+    updatedIngredients[type] = updatedCount;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+  };
+
+  removeIngredientHandler = (type) => {
+
   };
 
   render() {
     return (
       <Aux>
         <Burger ingredients={ this.state.ingredients } />
-        <BuildControls />
+        <BuildControls ingredientAdded={ this.addIngredientHandler }/>
       </Aux>
     );
   }
