c9c6b0bb4c40724dcd47efa65c7d71b76e24eca3
RaidenDilan <raiden18@me.com>
2020-02-10 02:03:40 +0000

    Adding Prop Type Validation

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import classes from './BurgerIngredient.css';
 
-const burgerIngredient = (props) => {
-  let ingredient = null;
+class BurgerIngredient extends Component {
+  render() {
+    let ingredient = null;
 
-  switch(props.type) {
-    case ('bread-bottom'):
+    switch(this.props.type) {
+      case ('bread-bottom'):
       ingredient = <div className={ classes.BreadBottom }></div>;
       break;
-    case ('bread-top'):
+      case ('bread-top'):
       ingredient = (
         <div className={ classes.BreadTop }>
           <div className={ classes.Seeds1 }></div>
@@ -31,9 +33,14 @@ const burgerIngredient = (props) => {
       break;
     default:
       ingredient = null;
+    }
 
     return ingredient;
   }
 };
 
-export default burgerIngredient;
+BurgerIngredient.propTypes = {
+  type: PropTypes.string.isRequired
+}
+
+export default BurgerIngredient;
