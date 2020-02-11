74aad0f9ad2c7f3df19ae92ae1479eb97484bf64
RaidenDilan <raiden18@me.com>
2020-02-11 00:58:49 +0000

    Showing & Hiding the Modal (with Animation)

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -28,7 +28,8 @@ class BurgerBuilder extends Component {
       meat: 0
     },
     totalPrice: 4,
-    purchasable: false
+    purchasable: false,
+    purchasing: false
   };
 
   updatePurchaseState(ingredients) {
@@ -41,6 +42,10 @@ class BurgerBuilder extends Component {
       }, 0);
 
       this.setState({ purchasable: sum > 0 })
+  };
+
+  purchaseHandler = () => {
+    this.setState({ purchasing: true });
   };
 
   addIngredientHandler = (type) => {
@@ -91,7 +96,7 @@ class BurgerBuilder extends Component {
     }
     return (
       <Aux>
-        <Modal>
+        <Modal show={ this.state.purchasing }>
           <OrderSummary ingredients={ this.state.ingredients }/>
         </Modal>
         <Burger ingredients={ this.state.ingredients } />
@@ -100,6 +105,7 @@ class BurgerBuilder extends Component {
           ingredientRemoved={ this.removeIngredientHandler }
           disabled={ disabledInfo }
           purchasable={ this.state.purchasable }
+          ordered={ this.purchaseHandler }
           price={ this.state.totalPrice }
           />
       </Aux>
