36e76cfd949f9c4d91afab83df3462b30e52aedc
RaidenDilan <raiden18@me.com>
2020-02-11 00:05:41 +0000

    Removing Ingredients Safely

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -46,14 +46,40 @@ class BurgerBuilder extends Component {
   };
 
   removeIngredientHandler = (type) => {
+    const oldCOunt = this.state.ingredients[type];
 
+    if (oldCOunt <= 0) return; // return nothing if oldCOunt is equal to or less than 0.
+    const updatedCount = oldCOunt - 1;
+    const updatedIngredients = {
+      // State should be updated in an unmutable way
+      // Then use the spread opertar to distribute the properties of the old ingredients state into the new object we are creatng bellow.
+      ...this.state.ingredients
+    };
+
+    updatedIngredients[type] = updatedCount;
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
   };
 
   render() {
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+      // { salad: true, meat: false, ... }
+    }
     return (
       <Aux>
         <Burger ingredients={ this.state.ingredients } />
-        <BuildControls ingredientAdded={ this.addIngredientHandler }/>
+        <BuildControls
+          ingredientAdded={ this.addIngredientHandler }
+          ingredientRemoved={ this.removeIngredientHandler }
+          disabled={ disabledInfo }
+          />
       </Aux>
     );
   }
