aeb006b7409f32f0ce6196b36a49b2e0ca16e1bc
RaidenDilan <raiden18@me.com>
2020-02-12 02:25:25 +0000

    Improving Performance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,40 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  // We're expecting to get back an Object format of the ingredients prop.
-  const ingredientSummary = Object.keys(props.ingredients)
-    .map(igKey => {
-      return (
-        <li key={ igKey }>
-          <span style={ { textTransform: 'capitalize' } }>{ igKey }</span>: { props.ingredients[igKey] }
-        </li>
-      );
-    });
+class OrderSummary extends Component {
+  // this could be a functional/stateless component, doesn't have to be a class based/stateful component
+  componentDidUpdate() {
+    console.log('[OrderSummary.js] componentDidUpdate');
+  }
 
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>
-        { ingredientSummary }
-      </ul>
-      <p><strong>Total Price: { props.price }</strong></p>
-      <p>Continue to Checkout?</p>
-      <Button clicked={ props.purchaseCanceled } btnType="Danger">CANCEL</Button>
-      <Button clicked={ props.purchasedContinued } btnType="Success">CONTINUE</Button>
-    </Aux>
-  );
+  render() {
+    // We're expecting to get back an Object format of the ingredients prop.
+    const ingredientSummary = Object.keys(this.props.ingredients)
+      .map(igKey => {
+        return (
+          <li key={ igKey }>
+            <span style={ { textTransform: 'capitalize' } }>{ igKey }</span>: { this.props.ingredients[igKey] }
+          </li>
+        );
+      });
+
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>
+          { ingredientSummary }
+        </ul>
+        <p><strong>Total Price: { this.props.price }</strong></p>
+        <p>Continue to Checkout?</p>
+        <Button clicked={ this.props.purchaseCanceled } btnType="Danger">CANCEL</Button>
+        <Button clicked={ this.props.purchasedContinued } btnType="Success">CONTINUE</Button>
+      </Aux>
+    );
+  }
 };
 
-orderSummary.propTypes = {
+
+OrderSummary.propTypes = {
   price: PropTypes.string.isRequired,
   ingredients: PropTypes.object.isRequired,
   purchaseCanceled: PropTypes.func.isRequired,
   purchasedContinued: PropTypes.func.isRequired,
 };
 
-export default orderSummary;
+export default OrderSummary;
