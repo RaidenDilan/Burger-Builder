ed2fe1f1128fb956a69819eebb76e2fdeed6a7ca
RaidenDilan <raiden18@me.com>
2020-02-15 23:38:38 +0000

    Navigating Back & To Next Page

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -12,9 +12,11 @@ const checkoutSummary = (props) => {
         <Burger ingredients={ props.ingredients } />
         <Button
           btnType='Danger'
+          clicked={ props.checkoutCancelled }
           >CANCEL</Button>
         <Button
           btnType='Success'
+          clicked={ props.checkoutContinued }
           >CONTINUE</Button>
       </div>
     </div>
