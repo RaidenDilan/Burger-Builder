f773db52d85511bb6b46c5a02e392c2a0f5adc3d
RaidenDilan <raiden18@me.com>
2020-02-16 02:14:49 +0000

    Fetching Orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 import Order from '../../components/Order/Order';
 
 class Orders extends Component {
   state = {
+    orders: [],
     loading: true
   }
 
   componentDidMount() {
+    // axios.get('/orders') // ENABLE: To test Orders component withErrorHandler(WrappedComponent)
+    axios.get('/orders.json')
+      .then(res => {
+        console.log(res);
 
+        // helper function
+        const fetchOrders = [];
+        for (let key in res.data) {
+          // fetchOrders.push(res.data[key]); // THIS: is fine but we can also...
+
+          // Push a new Object on this fetchOrders array,
+          // where we distribute the proeprties of the orders Object we fetch from FireBase (which is a spread operator)
+          // And add one new property of 'id' which is the 'key' => '-M0AiWYyBcnpQGaBn3US'.
+          fetchOrders.push({ ...res.data[key], id: key });
+          this.setState({ loaded: false, orders: fetchOrders });
+        }
+      })
+      .catch(err => {
+        this.setState({ loading: false });
+      });
   }
-  
+
   render() {
     return (
       <div>
@@ -21,4 +43,4 @@ class Orders extends Component {
   }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
