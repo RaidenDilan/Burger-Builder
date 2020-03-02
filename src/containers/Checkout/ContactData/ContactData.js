a358ef95922ea37262212ce8036d823af0db2bdb
RaidenDilan <raiden18@me.com>
2020-03-02 18:35:28 +0000

    Adding Order Actions

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -135,18 +135,6 @@ class ContactData extends Component {
       price: this.props.price,
       orderData: formData
     };
-
-    axios
-      .post('/orders.json', order)
-      .then(res => {
-        console.log('[ContactData.js] orderhandler => res', res);
-        this.setState({ loading: false });
-        this.props.history.push('/');
-      })
-      .catch(err => {
-        this.setState({ loading: false });
-        console.log('[ContactData.js] orderhandler => err', err);
-      });
   }
 
   inputChangedHandler = (event, inputIdentifier) => {
