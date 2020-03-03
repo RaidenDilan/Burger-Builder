60eee2e636e0a3f1eccb6b556040e6099c79c754
RaidenDilan <raiden18@me.com>
2020-03-03 00:18:51 +0000

    Fetching Orders (via Redux)

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,58 +1,49 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Order from '../../components/Order/Order';
 import axios from '../../axios-orders';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import * as actions from '../../store/actions/index';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 class Orders extends Component {
-  state = {
-    orders: [],
-    loading: true
-  }
-
   componentDidMount() {
-    // console.log('[Orders.js] componentDidMount ===>', this.props.orders);
-    // axios.get('/orders') // ENABLE: To test Orders component withErrorHandler(WrappedComponent)
-    axios
-      .get('/orders.json')
-      .then(res => {
-        // console.log('[Orders.js] componentDidMount res =>', res.data);
-        const fetchedOrders = [];
-
-        for (let key in res.data) {
-          // fetchedOrders.push(res.data[key]); // THIS: is fine but we can also...
-
-          // Push a new Object on this fetchedOrders array,
-          // where we distribute the proeprties of the orders Object we fetch from FireBase (which is a spread operator)
-          // And add one new property of 'id' which is the 'key' => '-M0AiWYyBcnpQGaBn3US'.
-
-          // Then we render our component we will essentially be using <Order key={ order.id } /> instead of FireBase 'id' - '-M0AiWYyBcnpQGaBn3US'
-          fetchedOrders.push({
-            ...res.data[key],
-            id: key
-          });
-        }
-
-        this.setState({ loading: false, orders: fetchedOrders });
-      })
-      .catch(err => {
-        console.log('[Orders.js] componentDidMount err =>', err);
-        this.setState({ loading: false });
-      });
+    // console.log('[Orders.js] componentDidMount ===>', this.props);
+    this.props.onFetchOrders();
   }
 
   render() {
+    let orders = <Spinner />;
+
+    if (!this.props.loading) {
+      orders = this.props.orders.map(order => (
+        <Order
+          key={ order.id }
+          ingredients={ order.ingredients }
+          price={ order.price } />
+        ));
+    }
+
     return (
       <div>
-        { this.state.orders.map(order => (
-          <Order
-            key={ order.id }
-            ingredients={ order.ingredients }
-            price={ order.price } />
-          )) }
+        { orders }
       </div>
     );
   }
 }
 
-export default withErrorHandler(Orders, axios);
+const mapStateToProps = state => {
+  return {
+    orders: state.order.orders,
+    loading: state.order.loading
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onFetchOrders: () => dispatch(actions.fetchOrders())
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(Orders, axios));
--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -28,18 +28,69 @@ export const purchaseBurger = (orderData) => {
     axios
       .post('/orders.json', orderData)
       .then(res => {
-        console.log('[order.js] action => res', res.data);
+        // console.log('[order.js] action => res', res.data);
         dispatch(purchaseBurgerSuccess(res.data.name, orderData));
       })
       .catch(err => {
-        console.log('[order.js] action => err', err);
+        // console.log('[order.js] action => err', err);
         dispatch(purchaseBurgerFail(err));
       });
   };
 };
 
-export const purchaseInit = () => {
+export const purchaseInit = (orders) => {
   return {
-    type: actionTypes.PURCHASE_INIT
+    type: actionTypes.PURCHASE_INIT,
+    orders: orders
+  };
+};
+
+export const fetchOrdersSuccess = (orders) => {
+  return {
+    type: actionTypes.FETCH_ORDERS_SUCCESS,
+    orders: orders
+  };
+};
+
+export const fetchOrdersFail = (error) => {
+  return {
+    type: actionTypes.FETCH_ORDERS_FAIL,
+    error: error
+  };
+};
+
+export const fetchOrdersStart = () => {
+  return {
+    type: actionTypes.FETCH_ORDERS_START
+  };
+};
+
+// async code
+export const fetchOrders = () => {
+  // RETURN A FUNCTION WHICH GETS DISPATCHED FIRST
+  return dispatch => {
+    dispatch(fetchOrdersStart());
+    axios
+      .get('/orders.json')
+      .then(res => {
+        const fetchedOrders = [];
+        for (let key in res.data) {
+          // console.log('key', key);
+          // fetchedOrders.push(res.data[key]); // THIS: is fine but we can also...
+          // Push a new Object on this fetchedOrders array,
+          // where we distribute the proeprties of the orders Object we fetch from FireBase (which is a spread operator)
+          // And add one new property of 'id' which is the 'key' => '-M0AiWYyBcnpQGaBn3US'.
+          // Then we render our component we will essentially be using <Order key={ order.id } /> instead of FireBase 'id' - '-M0AiWYyBcnpQGaBn3US'
+          fetchedOrders.push({
+            ...res.data[key],
+            id: key
+          });
+        }
+        dispatch(fetchOrdersSuccess(fetchedOrders));
+      })
+      .catch(error => {
+        // console.log('[Orders.js] componentDidMount error =>', error);
+        dispatch(fetchOrdersFail(error));
+      });
   };
 };
