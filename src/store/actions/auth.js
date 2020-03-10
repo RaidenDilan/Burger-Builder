d1f42ea1eb0301f351fc12ebe30cfdb4f4103e8f
RaidenDilan <raiden18@me.com>
2020-03-10 17:11:19 +0000

    Getting a Token from the Backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionTypes';
+import axios from 'axios';
 
 export const authStart = () => {
   return {
@@ -23,5 +24,22 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
   return dispatch => {
     dispatch(authStart());
+
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    };
+
+    axios
+      .post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${ process.env.REACT_APP_FIREBASE_API_KEY }`, authData)
+      .then(res => {
+        console.log('res', res);
+        dispatch(authSuccess(res.data));
+      })
+      .catch(err => {
+        console.log('err', err);
+        dispatch(authFail(err));
+      });
   };
 };
