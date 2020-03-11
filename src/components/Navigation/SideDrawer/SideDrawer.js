d81fd8cef7964b180988200b80ada964956e9dd2
RaidenDilan <raiden18@me.com>
2020-03-11 01:47:51 +0000

    Using updateObject in the Entire App

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -17,7 +17,9 @@ const sideDrawer = (props) => { // We are return something => {}
       <Backdrop
         show={ props.open }
         clicked={ props.closed } />
-      <div className={ attachedClasses.join(' ') } onClick={ props.closed }>
+      <div
+        className={ attachedClasses.join(' ') }
+        onClick={ props.closed }>
         <div className={ classes.Logo }>
           <Logo />
         </div>
